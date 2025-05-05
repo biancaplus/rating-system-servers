d9e85a54d1de9ec8b7f7e689abb643d25a7967e8
biancaplus <biancaplus1024@gmail.com>
2025-05-05 17:47:54 +0800

    add env variable

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "bing123456",
-  database: "rating_system",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // 测试数据库连接
